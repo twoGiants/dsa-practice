bb48369495c288720eb163bc82b8e13966b409ea
Stanislav Jakuschevskij <stas@two-giants.com>
2025-01-17 14:23:29 +0100

    Add slice equality to boilerplate test code

--- boilerplate/boilerplate_test.go
+++ boilerplate/boilerplate_test.go
@@ -10,6 +10,10 @@ func Test_XXX(t *testing.T) {
 	result := boilerplate.BoilerplateFunc(input)
 	expected := 0
 	if result != expected {
-		t.Fatalf("for input %v expected output %v, but got %v", input, expected, result)
+		t.Fatalf("for input %v expected output %d, but got %d", input, expected, result)
 	}
+	// expected := []int{}
+	// if !slices.Equal(result, expected) {
+	// 	t.Fatalf("for input %v expected output %v, but got %v", input, expected, result)
+	// }
 }
