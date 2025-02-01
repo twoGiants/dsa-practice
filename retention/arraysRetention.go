8a1c8e5ca34f4bee995495a8fb2176f64a6d4e0d
Stanislav Jakuschevskij <stas@two-giants.com>
2025-02-01 11:51:24 +0100

    Do retention practice with rotate-image

--- retention/arraysRetention.go
+++ retention/arraysRetention.go
@@ -2,6 +2,34 @@ package retention
 
 import "math"
 
+func RotateImage(matrix [][]int) {
+	left, right := 0, len(matrix)-1
+
+	for left < right {
+		top, bottom := left, right
+
+		for i := 0; i < right-left; i++ {
+			// save top left
+			topLeft := matrix[top][left+i]
+
+			// move bottom left to top left
+			matrix[top][left+i] = matrix[bottom-i][left]
+
+			// move bottom right to bottom left
+			matrix[bottom-i][left] = matrix[bottom][right-i]
+
+			// move top right to bottom right
+			matrix[bottom][right-i] = matrix[top+i][right]
+
+			// move top left to top right
+			matrix[top+i][right] = topLeft
+		}
+
+		left++
+		right--
+	}
+}
+
 func SetZeroes(matrix [][]int) {
 	ROWS, COLS := len(matrix), len(matrix[0])
 	rowZero := false
--- retention/arraysRetention_test.go
+++ retention/arraysRetention_test.go
@@ -7,6 +7,72 @@ import (
 	"testing"
 )
 
+func Test_RotateImage_TwoByTwoMatrix(t *testing.T) {
+	input := [][]int{
+		{1, 2},
+		{3, 4},
+	}
+	copy := [][]int{
+		{1, 2},
+		{3, 4},
+	}
+	expected := [][]int{
+		{3, 1},
+		{4, 2},
+	}
+
+	retention.RotateImage(input)
+
+	assert.MatrixEqual(t, input, expected, copy)
+}
+
+func Test_RotateImage_ThreeByThreeMatrix(t *testing.T) {
+	input := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	copy := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	expected := [][]int{
+		{7, 4, 1},
+		{8, 5, 2},
+		{9, 6, 3},
+	}
+
+	retention.RotateImage(input)
+
+	assert.MatrixEqual(t, input, expected, copy)
+}
+
+func Test_RotateImage_FourByFourMatrix(t *testing.T) {
+	input := [][]int{
+		{5, 1, 9, 11},
+		{2, 4, 8, 10},
+		{13, 3, 6, 7},
+		{15, 14, 12, 16},
+	}
+	copy := [][]int{
+		{5, 1, 9, 11},
+		{2, 4, 8, 10},
+		{13, 3, 6, 7},
+		{15, 14, 12, 16},
+	}
+	expected := [][]int{
+		{15, 13, 2, 5},
+		{14, 3, 4, 1},
+		{12, 6, 8, 9},
+		{16, 7, 10, 11},
+	}
+
+	retention.RotateImage(input)
+
+	assert.MatrixEqual(t, input, expected, copy)
+}
+
 func Test_SetZeroes_matrixWithOneZerosInLeftAndRightTopCorners_firstRowAndColumnAndLastColumnZeroes(t *testing.T) {
 	input := [][]int{
 		{0, 1, 2, 0},
