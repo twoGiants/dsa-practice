a996fbb7d79ebb95006500ccf89edb4887e69361
Stanislav Jakuschevskij <stas@two-giants.com>
2025-02-03 14:50:58 +0100

    Fix test bug with different result orders
    Iteration order of map keys is non deterministic in Go and because of
    that result and expected can not be compared item by item. To fix it
    both result and expected are sorted and then compared.

--- patterns/arrays/medium/group-anagrams/groupAnagrams_test.go
+++ patterns/arrays/medium/group-anagrams/groupAnagrams_test.go
@@ -3,8 +3,26 @@ package arrays_test
 import (
 	arrays "dsa/patterns/arrays/medium/group-anagrams"
 	"slices"
+	"sort"
 	"testing"
 )
+
+func sortGroups(groups [][]string) {
+	for _, group := range groups {
+		sort.Strings(group)
+	}
+	sort.Slice(groups, func(i, j int) bool {
+		if len(groups[i]) != len(groups[j]) {
+			return len(groups[i]) < len(groups[j])
+		}
+		for k := range groups[i] {
+			if groups[i][k] != groups[j][k] {
+				return groups[i][k] < groups[j][k]
+			}
+		}
+		return false
+	})
+}
 
 func Test_GroupAnagrams(t *testing.T) {
 	var anagramTests = []struct {
@@ -21,6 +39,9 @@ func Test_GroupAnagrams(t *testing.T) {
 		t.Run(at.name, func(t *testing.T) {
 
 			result := arrays.GroupAnagrams(at.input)
+
+			sortGroups(result)
+			sortGroups(at.expected)
 
 			for i := 0; i < len(at.expected); i++ {
 				if !slices.Equal(result[i], at.expected[i]) {
