baca0d6f04b21c6fc1d7bc220d22b40b3c0924db
Stanislav Jakuschevskij <stas@two-giants.com>
2025-02-22 13:42:12 +0100

    Implement docs boilerplate generation via CLI
    Add "create" and "delete" command processing which trigger the creation
    or deletion of the exercise boilerplate generation. So far for
    "docs.md", the rest follows.
    
    Extend basic CLI arguments validation. Will be refactored to a package
    like cobra.

--- main.go
+++ main.go
@@ -1,33 +1,83 @@
 package main
 
 import (
+	"dsa/utils/generator"
 	"errors"
 	"fmt"
 	"os"
+	"slices"
 )
 
 func main() {
 	fmt.Println("--- DSA Practice ---")
-	processArgs()
-}
-
-func processArgs() {
-	args := os.Args[1:]
-	if len(args) != 3 {
-		printUsage()
-		panic(errors.New("missing pattern, difficulty and title"))
+	info, err := processArgs()
+	if err != nil {
+		panic(err)
 	}
 
-	pattern := args[0]
+	tempDir := "temp"
+	if _, err := os.Stat(tempDir); err != nil {
+		panic(fmt.Errorf("%v, create %s and try again", err, tempDir))
+	}
+
+	if info.command == "create" {
+		tmplPath := "utils/generator/docs.gotmpl"
+		if err := generator.DocsBoilerplate(tempDir, tmplPath, info.pattern, info.difficulty, info.title); err != nil {
+			panic(err)
+		}
+	}
+
+	if info.command == "delete" {
+		if err := generator.DeleteExercise(tempDir, info.pattern, info.difficulty, info.title); err != nil {
+			panic(err)
+		}
+	}
+}
+
+type exercise struct {
+	command, pattern, difficulty, title string
+}
+
+func processArgs() (exercise, error) {
+	var anExercise exercise
+	args := os.Args[1:]
+	if len(args) != 4 {
+		printUsage()
+		return anExercise, errors.New("missing command, pattern, difficulty and title")
+	}
+
+	commands := []string{"create", "delete"}
+	command := args[0]
+	if !slices.Contains(commands, command) {
+		return anExercise, fmt.Errorf("invalid command: %s, allowed: %v", command, commands)
+	}
+	fmt.Println("command:", command)
+
+	patterns := []string{"arrays", "bit-manipulation", "stack"}
+	pattern := args[1]
+	if !slices.Contains(patterns, pattern) {
+		return anExercise, fmt.Errorf("invalid pattern: %s, allowed: %v", pattern, patterns)
+	}
 	fmt.Println("pattern:", pattern)
 
-	difficulty := args[1]
+	difficulties := []string{"easy", "medium", "hard"}
+	difficulty := args[2]
+	if !slices.Contains(difficulties, difficulty) {
+		return anExercise, fmt.Errorf("invalid difficulty: %s, allowed: %v", difficulty, difficulties)
+	}
 	fmt.Println("difficulty:", difficulty)
 
-	title := args[2]
+	title := args[3]
 	fmt.Println("title:", title)
+
+	return exercise{
+		command,
+		pattern,
+		difficulty,
+		title,
+	}, nil
 }
 
 func printUsage() {
-	fmt.Println("Usage: go run . <pattern> <difficulty> <title>")
+	fmt.Println("Usage: go run . <command> <pattern> <difficulty> <title>")
 }
