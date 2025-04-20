5ccaa0650a6df9ce2b45029e7e99598ee005824a
Stanislav Jakuschevskij <stas@two-giants.com>
2025-04-20 18:47:38 +0200

    Refactor theatre plays till big second phase
    Next the calculation and formatting will be separated.

--- refactoring/theatre/statement.go
+++ refactoring/theatre/statement.go
@@ -7,47 +7,111 @@ import (
 	"github.com/leekchan/accounting"
 )
 
+type Play struct {
+	Name string
+	Type string
+}
+
+type Invoice struct {
+	Customer     string
+	Performances []Performance
+}
+
+type Performance struct {
+	PlayID   string
+	Audience int
+}
+
 type StatementPrinter struct{}
 
-func (StatementPrinter) Print(invoice Invoice, plays map[string]Play) (string, error) {
-	totalAmount := 0
-	volumeCredits := 0
-	result := fmt.Sprintf("Statement for %s\n", invoice.Customer)
+func (s StatementPrinter) Print(invoice Invoice, plays map[string]Play) (string, error) {
+	return PlainTextStatement{plays, invoice}.Render()
+}
 
-	ac := accounting.Accounting{Symbol: "$", Precision: 2}
+type PlainTextStatement struct {
+	plays   map[string]Play
+	invoice Invoice
+}
 
-	for _, perf := range invoice.Performances {
-		play := plays[perf.PlayID]
-		thisAmount := 0
+func (s PlainTextStatement) Render() (string, error) {
+	result := fmt.Sprintf("Statement for %s\n", s.invoice.Customer)
 
-		switch play.Type {
-		case "tragedy":
-			thisAmount = 40000
-			if perf.Audience > 30 {
-				thisAmount += 1000 * (perf.Audience - 30)
-			}
-		case "comedy":
-			thisAmount = 30000
-			if perf.Audience > 20 {
-				thisAmount += 10000 + 500*(perf.Audience-20)
-			}
-			thisAmount += 300 * perf.Audience
-		default:
-			return "", fmt.Errorf("unknown type: %s", play.Type)
+	for _, perf := range s.invoice.Performances {
+		amount, err := s.amountFor(perf)
+		if err != nil {
+			return "", err
 		}
-
-		// add volume credits
-		volumeCredits += int(math.Max(float64(perf.Audience)-30, 0))
-		// add extra credit for every ten comedy attendees
-		if play.Type == "comedy" {
-			volumeCredits += int(math.Floor(float64(perf.Audience) / 5))
-		}
-
-		// print line for this order
-		result += fmt.Sprintf("  %s: %s (%d seats)\n", play.Name, ac.FormatMoney(float64(thisAmount)/100), perf.Audience)
-		totalAmount += thisAmount
+		result += fmt.Sprintf(
+			"  %s: %s (%d seats)\n",
+			s.playFor(perf).Name,
+			s.usd(amount),
+			perf.Audience,
+		)
 	}
-	result += fmt.Sprintf("Amount owed is %s\n", ac.FormatMoney(float64(totalAmount)/100))
-	result += fmt.Sprintf("You earned %d credits\n", volumeCredits)
+
+	totalAmount, err := s.totalAmount()
+	if err != nil {
+		return "", err
+	}
+	result += fmt.Sprintf("Amount owed is %s\n", s.usd(totalAmount))
+	result += fmt.Sprintf("You earned %d credits\n", s.totalVolumeCredits())
+	return result, nil
+}
+
+func (s PlainTextStatement) totalAmount() (int, error) {
+	result := 0
+	for _, perf := range s.invoice.Performances {
+		amount, err := s.amountFor(perf)
+		if err != nil {
+			return 0, err
+		}
+		result += amount
+	}
+	return result, nil
+}
+
+func (s PlainTextStatement) totalVolumeCredits() int {
+	result := 0
+	for _, perf := range s.invoice.Performances {
+		result += s.volumeCreditsFor(perf)
+	}
+	return result
+}
+
+func (PlainTextStatement) usd(number int) string {
+	ac := accounting.Accounting{Symbol: "$", Precision: 2}
+	return ac.FormatMoney(number / 100)
+}
+
+func (s PlainTextStatement) volumeCreditsFor(aPerformance Performance) int {
+	result := int(math.Max(float64(aPerformance.Audience)-30, 0))
+	// add extra credit for every ten comedy attendees
+	if s.playFor(aPerformance).Type == "comedy" {
+		result += int(math.Floor(float64(aPerformance.Audience) / 5))
+	}
+	return result
+}
+
+func (s PlainTextStatement) playFor(aPerformance Performance) Play {
+	return s.plays[aPerformance.PlayID]
+}
+
+func (s PlainTextStatement) amountFor(aPerformance Performance) (int, error) {
+	result := 0
+	switch s.playFor(aPerformance).Type {
+	case "tragedy":
+		result = 40000
+		if aPerformance.Audience > 30 {
+			result += 1000 * (aPerformance.Audience - 30)
+		}
+	case "comedy":
+		result = 30000
+		if aPerformance.Audience > 20 {
+			result += 10000 + 500*(aPerformance.Audience-20)
+		}
+		result += 300 * aPerformance.Audience
+	default:
+		return 0, fmt.Errorf("unknown type: %s", s.playFor(aPerformance).Type)
+	}
 	return result, nil
 }
