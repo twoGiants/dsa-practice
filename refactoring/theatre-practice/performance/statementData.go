9438453c7300590b3d03824add82155db6073cf7
Stanislav Jakuschevskij <stas@two-giants.com>
2025-04-26 18:14:10 +0200

    Move volume credits calculation to calculators

--- refactoring/theatre-practice/performance/statementData.go
+++ refactoring/theatre-practice/performance/statementData.go
@@ -15,11 +15,12 @@ type StatementData struct {
 }
 
 type enrichedPerformance struct {
-	playID   string
-	Name     string
-	Amount   int
-	Audience int
-	theType  string
+	playID        string
+	Name          string
+	Amount        int
+	Audience      int
+	theType       string
+	volumeCredits int
 }
 
 func CreateStatementData(invoice common.Invoice, plays map[string]common.Play) (StatementData, error) {
@@ -55,16 +56,54 @@ func (s StatementData) enrichPerformance(perf common.Performance) (enrichedPerfo
 		return enrichedPerformance{}, err
 	}
 	result.Amount = calculator.amount()
+	result.volumeCredits = calculator.volumeCredits()
 
 	return result, nil
 }
 
+func (s StatementData) playFor(perf enrichedPerformance) common.Play {
+	return s.plays[perf.playID]
+}
+
+func (s StatementData) totalAmount() int {
+	result := 0
+	for _, perf := range s.Performances {
+		result += perf.Amount
+	}
+	return result
+}
+
+func (s StatementData) totalVolumeCredits() int {
+	result := 0
+	for _, perf := range s.Performances {
+		result += perf.volumeCredits
+	}
+	return result
+}
+
 type performanceCalculator interface {
 	amount() int
+	volumeCredits() int
+}
+
+func createCalculator(perf enrichedPerformance) (performanceCalculator, error) {
+	calc := performanceCalculatorImpl{perf}
+	switch perf.theType {
+	case "tragedy":
+		return tragedyCalculatorImpl{calc}, nil
+	case "comedy":
+		return comedyCalculatorImpl{calc}, nil
+	default:
+		return nil, fmt.Errorf("unknown type: %s", perf.theType)
+	}
 }
 
 type performanceCalculatorImpl struct {
 	perf enrichedPerformance
+}
+
+func (p performanceCalculatorImpl) volumeCredits() int {
+	return int(math.Max(float64(p.perf.Audience)-30, 0))
 }
 
 type tragedyCalculatorImpl struct {
@@ -83,53 +122,15 @@ type comedyCalculatorImpl struct {
 	performanceCalculatorImpl
 }
 
-func (t comedyCalculatorImpl) amount() int {
+func (c comedyCalculatorImpl) amount() int {
 	result := 30000
-	if t.perf.Audience > 20 {
-		result += 10000 + 500*(t.perf.Audience-20)
+	if c.perf.Audience > 20 {
+		result += 10000 + 500*(c.perf.Audience-20)
 	}
-	result += 300 * t.perf.Audience
+	result += 300 * c.perf.Audience
 	return result
 }
 
-func createCalculator(perf enrichedPerformance) (performanceCalculator, error) {
-	calc := performanceCalculatorImpl{perf}
-	switch perf.theType {
-	case "tragedy":
-		return tragedyCalculatorImpl{calc}, nil
-	case "comedy":
-		return comedyCalculatorImpl{calc}, nil
-	default:
-		return nil, fmt.Errorf("unknown type: %s", perf.theType)
-	}
-}
-
-func (s StatementData) playFor(perf enrichedPerformance) common.Play {
-	return s.plays[perf.playID]
-}
-
-func (s StatementData) totalAmount() int {
-	result := 0
-	for _, perf := range s.Performances {
-		result += perf.Amount
-	}
-	return result
-}
-
-func (s StatementData) totalVolumeCredits() int {
-	result := 0
-	for _, perf := range s.Performances {
-		result += s.volumeCreditsFor(perf)
-	}
-	return result
-}
-
-func (StatementData) volumeCreditsFor(perf enrichedPerformance) int {
-	result := 0
-	result += int(math.Max(float64(perf.Audience)-30, 0))
-
-	if perf.theType == "comedy" {
-		result += int(math.Floor(float64(perf.Audience) / 5))
-	}
-	return result
+func (c comedyCalculatorImpl) volumeCredits() int {
+	return c.performanceCalculatorImpl.volumeCredits() + int(math.Floor(float64(c.perf.Audience)/5))
 }
